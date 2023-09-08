62aed66d6dcce976c0195cdebd9f8d25af2c8e34
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-09-08 18:00:30 +0200

    :sparkles: Scroller position

--- gsap-scrolltrigger-implementation/main.js
+++ gsap-scrolltrigger-implementation/main.js
@@ -12,6 +12,21 @@ gsap.from('.b2 h2', {
   scrollTrigger: {
     trigger: '.b2 h2',
 
+    // Trigger Scroller
+    // start: 'top bottom', // <- default values
+
+    // start: 'top bottom-=200', // <- default values - 200px (not the best choise: responsive?)
+
+    // start: 'top 50%',
+    start: 'top center+=10%',
+
+    // end: 'bottom top', // <- default values
+
+    // end: 'bottom top-=200', // <- default values - 200px (not the best choise: responsive?)
+
+    // end: 'bottom 50%',
+    end: 'bottom top+=10%',
+
     // For development environment
     markers: true
   }
